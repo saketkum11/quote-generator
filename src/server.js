c983e6f0c7e312086d8035559a43310869f41870
Saket kumar <kumarsaket601@gmail.com>
2024-01-05 19:36:02 +0530

    feat: export file store

---
+++ src/server.js
@@ -0,0 +1,6 @@
+import { Router } from "./Router/Router";
+import { appStore } from "./appStore/appStore";
+import { Home } from "./pages/Home";
+import { Card } from "./component/Card";
+// export
+export { Router, appStore, Home, Card };
