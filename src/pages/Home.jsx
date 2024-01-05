b95ef26965e4ba9897ff86e9e724efe476f9ec9e
Saket kumar <kumarsaket601@gmail.com>
2024-01-05 19:35:29 +0530

    style: created css file

---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+import "../style/Home.css";
+import { Card } from "../server";
+const Home = () => {
+  return (
+    <div className="home-page">
+      <header>
+        <div className="home-page-header-container ">
+          <nav className="home-page-nav ">
+            <div className="home-page-navigate ">
+              <div>
+                <Link className="home-page-text home-page-text-bold" to="/">
+                  Home
+                </Link>
+              </div>
+              <div>
+                <Link className="home-page-text " to="/bookmark">
+                  Bookmark
+                </Link>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+      <main>
+        <section>
+          <Card />
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export { Home };
